b2c9a7db6bdb19987891ffdf0a695fd75e664c25
noodlemoodle <zhang.qi.yue@hotmail.com>
2019-02-26 21:42:26 -0500

    Updates

---
+++ precache-manifest.71582e63ffc89f62261d04b0e6cc7cf6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ae9795df96298d56a555b7a69becf2",
+    "url": "/home1/static/media/bus.d5ae9795.png"
+  },
+  {
+    "revision": "c781bac1ac98a827008c",
+    "url": "/home1/static/css/main.d1c0980c.chunk.css"
+  },
+  {
+    "revision": "6d224d91d77261269a94",
+    "url": "/home1/static/js/runtime~main.6d224d91.js"
+  },
+  {
+    "revision": "bd6c2d14195bb5289ed7abdf05c5ae49",
+    "url": "/home1/static/media/resume.bd6c2d14.pdf"
+  },
+  {
+    "revision": "698bc9a770cd8d642889",
+    "url": "/home1/static/js/2.698bc9a7.chunk.js"
+  },
+  {
+    "revision": "55de2329141e9645aaa5647437bddaed",
+    "url": "/home1/static/media/noimage.55de2329.jpg"
+  },
+  {
+    "revision": "86494529c0b0984545f0df81bc2e64d0",
+    "url": "/home1/static/media/avatar.86494529.png"
+  },
+  {
+    "revision": "d3700680f1d745efb4ebbf0202f71e09",
+    "url": "/home1/static/media/589proj.d3700680.png"
+  },
+  {
+    "revision": "cc140dda84fb47d0806908af933e7103",
+    "url": "/home1/static/media/bspline.cc140dda.png"
+  },
+  {
+    "revision": "c781bac1ac98a827008c",
+    "url": "/home1/static/js/main.c781bac1.chunk.js"
+  },
+  {
+    "revision": "245bbb61d0969b35e622c315e407d5f5",
+    "url": "/home1/static/media/chess.245bbb61.png"
+  },
+  {
+    "revision": "512ddc4991fe8dc777f8378819492890",
+    "url": "/home1/static/media/flashcards.512ddc49.png"
+  },
+  {
+    "revision": "eec3f610a6c4aee579569a73c64aa5da",
+    "url": "/home1/static/media/mondrian.eec3f610.png"
+  },
+  {
+    "revision": "10ed53d3b7509e7c369da7487f4c97a5",
+    "url": "/home1/static/media/poisson.10ed53d3.png"
+  },
+  {
+    "revision": "0bca85a4c336a1d567827966d19c67dc",
+    "url": "/home1/static/media/prolog.0bca85a4.png"
+  },
+  {
+    "revision": "22acb18f732c8a45db4c8861f036984f",
+    "url": "/home1/static/media/471proj.22acb18f.png"
+  },
+  {
+    "revision": "137c9c3ab96421a190e6f023d1eefcbf",
+    "url": "/home1/static/media/513proj.137c9c3a.png"
+  },
+  {
+    "revision": "698bc9a770cd8d642889",
+    "url": "/home1/static/css/2.7e0d886a.chunk.css"
+  },
+  {
+    "revision": "1c6dac2e113f49b70feabda25cbc7c6b",
+    "url": "/home1/index.html"
+  }
+];
